4035cd749d168dd73b2cbef7c6704785d5996845
Ubuntu <ubuntu@ip-172-31-82-149.ec2.internal>
2019-06-08 19:38:26 +0000

    use Calculator object & use beforeEach()

--- myTest/scripts/calc.js
+++ myTest/scripts/calc.js
@@ -1,10 +1,26 @@
-function addition(a, b) {
-    if (typeof(a) == 'number' && typeof(b) == 'number') {
-        return a + b;
-    }else{
-        // return 'ERROR!!!'
-        alert('ERROR!!!');
-        
+//creating Calculator object
+Calculator = function() {
+    this.value = 0;
+};
+
+
+//adding 'add' function to the Calculator obj
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("ERROR!");
     }
-}
+};
+
+
+// function addition(a, b) {
+//     if (typeof(a) == 'number' && typeof(b) == 'number') {
+//         return a + b;
+//     }else{
+//         // return 'ERROR!!!'
+//         alert('ERROR!!!');
+        
+//     }
+// }
 
--- myTest/spec/calcSpec.js
+++ myTest/spec/calcSpec.js
@@ -1,23 +1,37 @@
 //*** spec/calcSpec.js
 
-describe('Calculator', function(){
-    
-    describe('Addition function', function(){
-        it('should return 42', function(){
-            expect(addition(20,22)).toBe(42);
+describe('Calculator', function() {
+
+    //create an instance of Calculator obj before each test
+    beforeEach(function() {
+        calc = new Calculator;
+    });
+
+
+    describe('Addition function', function() {
+        it('should return 42', function() {
+            // expect(addition(20,22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
-        it('should return 25', function(){
-            expect(addition(20,5)).toBe(25);
+        it('should return 25', function() {
+            // expect(addition(20,5)).toBe(25);
+            calc.add(20);
+            calc.add(5);
+            expect(calc.value).toBe(25);
         });
         // it('should return an ERROR if user dont supply 2 numbers', function(){
         //     expect(addition('one', 'two')).toBe('ERROR!!!');
         // })
-        
-        //use spyOn on window.alert() event
-        it('should return an ERROR if user dont supply 2 numbers', function(){
-            spyOn(window,'alert');
-            addition('one', 'two');
-            expect(window.alert).toHaveBeenCalledWith('ERROR!!!');
+
+        //use spyOn on window.alert() event 
+        it('should return an ERROR if user dont supply 2 numbers', function() {
+            spyOn(window, 'alert');
+            // addition('one', 'two');
+            // expect(window.alert).toHaveBeenCalledWith('ERROR!');
+            calc.add('one');
+            expect(window.alert).toHaveBeenCalledWith('ERROR!');
         })
     });
 })
